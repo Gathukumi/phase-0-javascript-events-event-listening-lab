e6fb5dde346365ac708cdbc6a35702faa84bf822
Gathukumi <gathukumidavid001@gmail.com>
2023-10-08 17:23:28 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    // Get the element with the ID 'input'
+    const input = document.getElementById('input');
+  
+    // Define the callback function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add a click event listener to the 'input' element
+    input.addEventListener('click', clickAlert);
 }
+  
+  // Call the addingEventListener function to set up the event listener
+  addingEventListener();
+  
